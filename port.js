1c5cbc5e66a63d9f12e092ea8286bddbc9395312
Dev-ayo10 <ernest.dev10@gmail.com>
2025-07-22 16:34:15 +0100

    new commits to Post application

--- port.js
+++ port.js
@@ -133,15 +133,16 @@
      spinner.style.display = "none";
       //now we can show the output to the user
       output.innerHTML = `
-      <div> <svg class="check" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#18333C"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg> </div>
-      <p>Hello Your request has been received succesfully.</p>`;
+      <div class="check"> <svg class="check" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#18333C"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg> </div>
+      <p class="fn-msg">Hello Your request has been received succesfully.</p>`;
+      console.log("Success: ", finalPost);
       // Hide the form and show only the output
       form.style.display = "none";
       output.style.display = "block";
     } catch(error){
       console.log("Error: ",error );
       spinner.style.display= "none";
-      output.innerHTML = `<p>hello try again something went wrong</p>`
+      output.innerHTML = `<p>Failed to Send messageCheck your network connection</p>`
     }
     form.reset();
    });
